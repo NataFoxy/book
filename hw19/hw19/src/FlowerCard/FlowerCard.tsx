4a3e3b6939221ec5a8cf3f6a8740064ac9dd99c7
Natalija <natalijaallahverdijeva@gmail.com>
2023-07-26 23:18:01 +0200

    fixed

--- hw19/hw19/src/FlowerCard/FlowerCard.tsx
+++ hw19/hw19/src/FlowerCard/FlowerCard.tsx
@@ -18,19 +18,9 @@ export default function FlowerCard({ flower }: Props): JSX.Element {
   return (
     <div className={style.card}>
       <h2 className={style.header}>{title}</h2>
-      <div className={style.info}>
-        <img
-          src="https://live.staticflickr.com/65535/49842111702_ac23230a2d_b.jpg"
-          alt="lily of the valley"
-          width={300}
-        />
-      </div>
-
-      <div className={style.info} style={{ backgroundColor: color }}>
-        {color}
-      </div>
-      <div className={style.info}>family of: {family}</div>
-      <div className={style.info}>Price: {price} Euro</div>
+      <div style={{ backgroundColor: color }}>{color}</div>
+      <div>family of: {family}</div>
+      <div>Price: {price} Euro</div>
     </div>
   );
 }
--- hw19/hw19/src/Parent/Parent.tsx
+++ hw19/hw19/src/Parent/Parent.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FlowerCard from '../FlowerCard/FlowerCard';
+import style from './Parent.module.css';
 
 export default function Parent(): JSX.Element {
   const flower = {
@@ -8,9 +9,25 @@ export default function Parent(): JSX.Element {
     family: 'asparagus',
     price: 4,
   };
+
+  const flower1 = {
+    color: 'red',
+    title: 'Rose',
+    family: 'rosa',
+    price: 8,
+  };
+
+  const flower2 = {
+    color: 'lavender',
+    title: 'Lily',
+    family: 'liliaceae',
+    price: 12,
+  };
   return (
-    <div>
+    <div className={style.container}>
       <FlowerCard flower={flower} />
+      <FlowerCard flower={flower1} />
+      <FlowerCard flower={flower2} />
     </div>
   );
 }
